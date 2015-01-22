7d94a19914e6f4f6c3eb3c2031a424a547ad67d5
Paul Stadig <paul@stadig.name>
2015-01-22 14:05:09 -0500

    Move clojure and clojurescript into dev dependencies.

--- project.clj
+++ project.clj
@@ -5,13 +5,11 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :deploy-repositories [["releases" :clojars]]
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2371"]
-                 [prismatic/schema "0.3.0"]
-                 [com.cognitect/transit-clj "0.8.259"]
-                 [com.cognitect/transit-cljs "0.8.192"]]
-  :plugins [[lein-cljsbuild "1.0.3"]
-            [com.cemerick/clojurescript.test "0.3.1"]]
+  :dependencies [[com.cognitect/transit-clj "0.8.259"]
+                 [com.cognitect/transit-cljs "0.8.192"]
+                 [prismatic/schema "0.3.0"]]
+  :plugins [[com.cemerick/clojurescript.test "0.3.3"]
+            [lein-cljsbuild "1.0.3"]]
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
   :cljsbuild {:test-commands {"unit" ["node" :node-runner
@@ -30,5 +28,7 @@
                                      "src/cljs" "test/cljs"]
                       :compiler {:output-to "target/unit-test.js"
                                  :optimizations :simple}}}}
-  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.1.3"]]
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.1.3"]
+                                  [org.clojure/clojure "1.6.0"]
+                                  [org.clojure/clojurescript "0.0-2371"]]
                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}})
