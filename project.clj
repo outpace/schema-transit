a224d045fd7c0497026b8d7a224d68a4d467d3b6
Paul Stadig <paul@stadig.name>
2015-02-20 15:08:46 -0500

    Prepare for next release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.outpace/schema-transit "0.2.2"
+(defproject com.outpace/schema-transit "0.2.3-SNAPSHOT"
   :description "A library for serializing Prismatic Schema definitions with
   Transit."
   :url "http://github.com/outpace/schema-transit"
